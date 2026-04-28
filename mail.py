65415e7fab9b8a2675f8b2ee7a41adb05b65ccad
Kushal Pathak <Pathak.kushal.321@gmail.com>
2026-04-28 10:12:07 +0530

    send username instead of name

--- mail.py
+++ mail.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 conf=ConnectionConfig(MAIL_USERNAME=os.getenv("app_mail"), MAIL_PASSWORD=os.getenv("app_password"), MAIL_FROM=os.getenv("app_mail"), MAIL_PORT=587, MAIL_SERVER="smtp.gmail.com", MAIL_STARTTLS=True, MAIL_SSL_TLS=False, USE_CREDENTIALS=True)
 
-async def send_email_async(to_email, username, password):
+async def send_email_async(to_email, username, password, name):
     message=MessageSchema(subject="Registration confirmation", recipients=[to_email], body=f"""
-    Hello {username},
+    Hello {name},
 
 Your faculty account has been created.
 
